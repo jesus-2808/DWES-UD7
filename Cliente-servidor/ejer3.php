89de191fb6c641b337f382ade3f7aed9116d3da8
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2022-02-04 11:01:10 +0100

    pdte arreglar conexion BD

---
+++ Cliente-servidor/ejer3.php
@@ -0,0 +1,48 @@
+<?php //IMPORTANTE: ELIMINA EL ESPACIO ANTES DE LA INTERROGACIÓN
+// Vaciamos algunas variables
+$error = "";
+$resultado = "";
+
+
+// Iniciamos el cliente SOAP
+// Escribimos la dirección donde se encuentra el servicio
+$url = "http://192.168.129.193/DWES-UD7/Cliente-servidor/index1.php";
+$uri = "http://192.168.129.193/DWES-UD7/Cliente-servidor";
+$cliente = new SoapClient(null, array('location' => $url, 'uri' => $uri));
+
+// Ejecutamos las siguientes líneas al enviar el formulario
+if (isset($_POST['enviar'])) {
+    // Establecemos los parámetros de envío
+    if (!empty($_POST['poblacion']) && !empty($_POST['poblacion']))  {
+    
+        // Si los parámetros son correctos, llamamos a la función letra de suma.php
+        $resultado = $cliente->getCiudad($_POST['poblacion']);
+       var_dump($resultado);
+    } else {
+        $error = "<strong>Error:</strong> resultado incorrecto, formato no soportado<br/><br/>Ej: 1";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Calculo de suma</title>
+        <link rel="stylesheet" type="text/css" href="/estilo.css">
+    </head>
+<body>
+   
+<form action="ejer3.php" method="post">
+    <?php //IMPORTANTE: ELIMINA EL ESPACIO ANTES DE LA INTERROGACIÓN
+     
+      print "<input type='number' name='poblacion'>";
+  
+      print "<input type='submit' name='enviar' value='Calcular numero'>";
+      print "<p class='error'>$error</p>";
+    
+  ?>
+    
+    </form>
+  
+</body>
+</html>
---
+++ ejer2.php
@@ -0,0 +1,18 @@
+<?php //IMPORTANTE: ELIMINA EL ESPACIO ANTES DE LA INTERROGACIÓN
+// Instanciamos un nuevo servidor SOAP
+$uri="http://192.168.129.193/DWES-UD7";
+$server = new SoapServer(null,array('uri'=>$uri));
+$server->addFunction("par");
+$server->handle();
+
+// Función para obtener si el numero es par
+function par($num) {
+    if ($num%2==0){
+        return true;
+        
+    }else{
+        return false;
+    }
+  
+}
+?>
